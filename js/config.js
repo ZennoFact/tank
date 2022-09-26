2162a636485b5809b3384efc8d48460cfac5fcc7
ZennoFact <zenno04.g@gmail.com>
2022-09-26 09:15:29 +0900

    FPSモードでの視点のずれを修正

---
+++ js/config.js
@@ -0,0 +1,31 @@
+const config = {
+  button: {
+    a: 0,
+    b: 1,
+    x: 2,
+    y: 3,
+    lb: 4,
+    rb: 5,
+    lt: 6,
+    rt: 7,
+    back: 8,
+    start: 9,
+  },
+  stick: {
+    leftX: 0,
+    leftY: 1,
+    rightX: 2,
+    rightY: 3,
+  },
+  // 現状使用しないがキーボード操作にはこれを使用
+  key: {
+    foward: "w",
+    left: "a",
+    right: "d",
+    back: "s",
+    shoot: " ",
+    edit: "e",
+    fps: "f",
+    none: "",
+  },
+};
