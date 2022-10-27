708e9518e30abffba33a2b043d518f130c4f39af
ZennoFact <zenno04.g@gmail.com>
2022-10-27 13:23:59 +0900

    ゲームマスターが床の割合をカウントする基本処理を作成

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,5 +1,7 @@
-function loadUI() {
-  const clone = document.querySelector("#uiParts").content.cloneNode(true);
+function loadUI(type = "player") {
+  const domId = type === "player" ? "#uiParts" : "#gmParts";
+  // TODO: css含めたgmパーツの表示から
+  const clone = document.querySelector(domId).content.cloneNode(true);
   const uiCollection = clone.querySelectorAll(".ui");
 
   Array.prototype.forEach.call(uiCollection, (item) => {
